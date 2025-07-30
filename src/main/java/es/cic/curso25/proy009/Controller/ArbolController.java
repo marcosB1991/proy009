e7872690062d6a2c429cc803c0cf44e633075778
Marcos <marcos@marcos>
2025-07-30 13:34:36 +0200

    Se  añade crear rama

--- src/main/java/es/cic/curso25/proy009/Controller/ArbolController.java
+++ src/main/java/es/cic/curso25/proy009/Controller/ArbolController.java
@@ -6,11 +6,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.cic.curso25.proy009.Model.Arbol;
+import es.cic.curso25.proy009.Model.Rama;
 import es.cic.curso25.proy009.Service.ArbolService;
 
 @RestController
@@ -39,5 +41,15 @@ public class ArbolController {
         Arbol arbol= arbolService.crearArbolyRamas(especie, numRamas);
         return ResponseEntity.ok(arbol);
     }
+
+    @PostMapping("/{id}/rama")
+    public ResponseEntity <Arbol> agregarRama(@PathVariable Long id, @RequestBody Rama ramaAgregada)
+    //Se usa requestbody porque estoy pasando un objeto request param lo uso para int boolean string, etc..
+    {
+        Arbol arbol = arbolService.agregarRama(id, ramaAgregada);
+        return ResponseEntity.ok(arbol);
+    }
+
+    
 
 }
